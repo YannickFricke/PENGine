a1269e5058cd48cc2cefecf665ac804b45bc1549
Yannick Fricke <yannickfricke@googlemail.com>
2022-04-10 00:08:38 +0200

    Added the "Logger" type alias

---
+++ .changeset/sixty-tools-kneel.md
@@ -0,0 +1,8 @@
+---
+'@pengine/logging': minor
+---
+
+Added the "Logger" type alias
+
+The "Logger" type aliases the `Debugger` interface from
+the `debug` module.
--- packages/libs/logging/src/index.ts
+++ packages/libs/logging/src/index.ts
@@ -1,5 +1,7 @@
 import createDebugLogger, { Debugger } from 'debug';
 
+export type Logger = Debugger;
+
 /**
  * Creates a debug logger with the given name.
  *
@@ -7,6 +9,6 @@ import createDebugLogger, { Debugger } from 'debug';
  * @param {string} loggerName The name of the logger.
  * @return {Debugger} The debug logger.
  */
-export function createLogger(loggerName: string): Debugger {
+export function createLogger(loggerName: string): Logger {
 	return createDebugLogger(loggerName);
 }
