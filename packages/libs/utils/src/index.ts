be299b3c102c315b999855bd118a2127d67af19f
Yannick Fricke <yannickfricke@googlemail.com>
2022-04-10 05:29:13 +0200

    Moved the deepMerge function to the objects.ts file

--- packages/libs/utils/src/index.ts
+++ packages/libs/utils/src/index.ts
@@ -1,17 +1,1 @@
-import merge from 'ts-deepmerge';
-
-/**
- * Merges two objects or more objects deeply
- *
- * @export
- * @template T The type of the objects to merge
- * @param {T} target The target object
- * @param {...Partial<T>[]} sources The source objects
- * @return {T} The merged object
- */
-export function deepMerge<T extends Record<string, any>>(
-	target: T,
-	...sources: Partial<T>[]
-): T {
-	return merge(target, ...sources) as T;
-}
+export * from './objects';
